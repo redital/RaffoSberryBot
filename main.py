b7302aeee54755fbca40937a901b964d3cf7fc83
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2023-04-16 11:35:19 +0200

    Bug fix

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ mode = "Hub"
 def start(message):
     markup=types.ReplyKeyboardRemove()
 
-    bot.send_message( message.chat.id,
-    """\
-    Bot realizzato per il controllo del fantastico RaffoSberry
-    Se non sai cosa posso fare digita (o premi) /hub !
-    """ , markup)
+    bot.send_message( 
+        message.chat.id,
+        """\
+Bot realizzato per il controllo del fantastico RaffoSberry
+Se non sai cosa posso fare digita (o premi) /help !
+        """, 
+        reply_markup=markup)
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -25,54 +27,61 @@ def help(message):
 
     messaggiHelp = {}
     messaggiHelp["Hub"] = """\
-    Bot realizzato per il controllo del fantastico RaffoSberry.
-    Adesso ti trovi nell'hub, digita (o premi) /media per entrare nella modalità media in cui portrai visualizzare i media presenti in una evenutale periferica collegata e riprorne il contenuto
+Bot realizzato per il controllo del fantastico RaffoSberry.
+Adesso ti trovi nell'hub, digita (o premi) /media per entrare nella modalità media in cui portrai visualizzare i media presenti in una evenutale periferica collegata e riprorne il contenuto
     """
     messaggiHelp["Media"] = """\
-    Bot realizzato per il controllo del fantastico RaffoSberry.
-    Adesso ti trovi in modalità media in cui portrai visualizzare i media presenti in una evenutale periferica collegata e riprorne il contenuto. 
-    Digita (o premi) /hub per tornare all'hub
+Bot realizzato per il controllo del fantastico RaffoSberry.
+Adesso ti trovi in modalità media in cui portrai visualizzare i media presenti in una evenutale periferica collegata e riprorne il contenuto. 
+Digita (o premi) /hub per tornare all'hub
     """
 
-    bot.send_message( message.chat.id, messaggiHelp[mode], reply_markup=markup )
+    bot.send_message(message.chat.id, messaggiHelp[mode], reply_markup=markup)
 
     
 @bot.message_handler(commands=['hub'])
 def hub(message):
+    global mode
     markup=types.ReplyKeyboardRemove()
 
     if mode == "Hub":
-        bot.send_message( message.chat.id,
-        """\
-        Sei già nell'hub
-        """ , markup)
+        bot.send_message( 
+            message.chat.id,
+            "Sei già nell'hub", 
+            reply_markup=markup
+            )
     else:
-        bot.send_message( message.chat.id,
-        """\
-        Adesso sei nell'hub. 
-        Se non sai cosa fare qui usa l'help!
-        """ , markup)
+        mode = "Hub"
+        bot.send_message(
+            message.chat.id,
+            "Adesso sei nell'hub.\nSe non sai cosa fare qui usa l'help!", 
+            reply_markup=markup
+            )
     
 @bot.message_handler(commands=['media'])
 def media(message):
+    global mode
     markup=types.ReplyKeyboardRemove()
 
-    if mode == "media":
-        bot.send_message( message.chat.id,
-        """\
-        Sei già in modalità media
-        """ , markup)
-    elif mode != "hub":
-        bot.send_message( message.chat.id,
-        """\
-        Solo dall'hub si può cambiare modalità
-        """ , markup)
+    if mode == "Media":
+        bot.send_message( 
+            message.chat.id,
+            "Sei già in modalità media", 
+            reply_markup=markup
+            )
+    elif mode != "Hub":
+        bot.send_message( 
+            message.chat.id,
+            "Solo dall'hub si può cambiare modalità", 
+            reply_markup=markup
+            )
     else:
-        bot.send_message( message.chat.id,
-        """\
-        Adesso sei in modalità media. 
-        Se non sai cosa fare qui usa l'help!
-        """ , markup)
+        mode="Media"
+        bot.send_message(
+            message.chat.id,
+            "Adesso sei in modalità media.\nSe non sai cosa fare qui usa l'help!", 
+            reply_markup=markup
+            )
 
 
 
