83618c451cd35456ccb3793d3fcb1bfb35301780
Raffosberry <>
2023-12-13 16:24:22 +0100

    Tolte stampe di debug

--- RaffoSberryBot.py
+++ RaffoSberryBot.py
@@ -49,13 +49,7 @@ def reset(message):
 
 def isAuthenticated(message):
     now =  datetime.fromtimestamp(message.date)
-    print(type(now))
     delta = now - lastActivity
-    print(type(delta))
-    print((delta))
-    print(type(inactivityTime))
-    print((inactivityTime))
-    print()
     if delta > inactivityTime :
         global autenticato
         autenticato = False
